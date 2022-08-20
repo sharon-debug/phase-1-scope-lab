414b7382475832f803a641bfa01bf261ebe0061c
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-08-20 21:59:07 +0300

    completed assgnment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+    return customerName;
+
+    
+}
+var setBestCustomer;
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(parameter='maybe bob'){
+    return bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    const changeLeastFavoriteCustomer = "Vane";
+    changeLeastFavoriteCustomer = "Purity";
+    return changeLeastFavoriteCustomer;
+ 
+ }
